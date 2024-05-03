3ccdc15f0487ca6324ed502734615ea4de233c90
lvmartin <martin.l2@student.unimelb.edu.au>
2024-05-03 22:58:34 +1000

    Add a decision tree training model

--- program.py
+++ program.py
@@ -309,3 +309,38 @@ train, test = train_test_split(discretizedData, test_size=0.2, random_state=42, 
 # save the train and test file
 train.to_csv('datasets/train.csv', index=False)
 test.to_csv('datasets/text.csv', index=False)
+
+# Create a training model
+numericData = allData
+"""
+OPT 1: Decision Tree
+"""
+# Change string values into numerical values
+unique_countries = users['User-Country'].unique()
+countries = {value: idx for idx, value in enumerate(unique_countries)}
+print(countries)
+numericData['User-Country'] = numericData['User-Country'].map(countries)
+
+unique_publishers = books['Book-Publisher'].unique()
+publishers = {value: idx for idx, value in enumerate(unique_publishers)}
+print(publishers)
+numericData['Book-Publisher'] = numericData['Book-Publisher'].map(publishers)
+
+# Separate the feature columns from the target column.
+# Target variable (discrete) [y] : rating
+# columns [x1, x2...]: attributes / predictors
+# Create a predictive model [f(x...)]
+attributes = ['User-Age', 'User-Country', 'Book-Publisher', 'Year-Of-Publication']
+
+X = numericData[attributes]
+y = numericData['Book-Rating']
+
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, y)
+
+tree.plot_tree(dtree, feature_names=attributes)
+
+"""
+OPT 2: k Nearest Neighbour
+"""
+
