6aeeaa2d40971a941934dce68ffa8456e38200d4
lvmartin <martin.l2@student.unimelb.edu.au>
2024-05-07 21:55:35 +1000

    Ensure resulting csvs are not saved to the repo as they can be easily obtained by running the code locally

--- modeldata.py
+++ modeldata.py
@@ -25,20 +25,20 @@ discreteAgeLabels = list(discreteAges.keys())
 discreteAgeValues = list(discreteAges.values())
 matrix['User-Age'] = pd.cut(matrix['User-Age'], discreteAgeValues, labels=discreteAgeLabels[:-1])
 matrix = matrix.rename(columns={"User-Age": "User-Generation"})
-matrix.to_csv('datasets/matrix.csv')
+matrix.to_csv('results/matrix.csv')
 
 rating_from_generation = all_rating_data.groupby(['ISBN', 'User-Generation'])['Book-Rating'].mean().reset_index()
 rating_from_generation['Book-Rating'] = rating_from_generation['Book-Rating'].fillna(0)
-rating_from_generation.to_csv('datasets/rating_from_generation.csv')
+rating_from_generation.to_csv('results/rating_from_generation.csv')
 
 
 favourite_generation_for_book = rating_from_generation.loc[rating_from_generation.groupby('ISBN')['Book-Rating'].idxmax()]
 favourite_generation_for_book = favourite_generation_for_book.rename(columns={"User-Generation": "Favourite-Generation"})
-favourite_generation_for_book.to_csv('datasets/favourite_rating.csv')
+favourite_generation_for_book.to_csv('results/favourite_rating.csv')
 
 new_matrix = pd.merge(books, favourite_generation_for_book[['ISBN', 'Favourite-Generation']], on='ISBN', how='inner')
 new_matrix = new_matrix.drop(columns=['Year-Of-Publication', 'ISBN'])
 new_matrix['Book-Title'] = new_matrix['Book-Title'].str.replace('[{}]'.format(string.punctuation), '', regex=True)
 new_matrix['Book-Title'] = new_matrix['Book-Title'].str.split().str.len()
 new_matrix = new_matrix.rename(columns={"Book-Title": "Book-Title-Length"})
-new_matrix.to_csv('datasets/new_matrix.csv')
+new_matrix.to_csv('results/new_matrix.csv')
